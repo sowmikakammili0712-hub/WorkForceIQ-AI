11cd50f93a2c6c05710a3ecf157676fc46a03bb5
sowmikakammili0712-hub <sowmikakammili.0712@gmail.com>
2026-06-22 20:20:00 +0530

    Sprint 3 - Ticket lifecycle and operations metrics

--- 06_Python/DataGenerator.py
+++ 06_Python/DataGenerator.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from faker import Faker
 import random
+from datetime import datetime, timedelta
 
 print("===================================")
 print("WorkForceIQ AI")
@@ -55,6 +56,19 @@ statuses = [
     "Resolved",
     "Open"
 ]
+queues = [
+    "Customer Support",
+    "Billing Support",
+    "Technical Support",
+    "Domain Support",
+    "Hosting Support"
+]
+
+tiers = [
+    "Tier1",
+    "Tier2",
+    "Tier3"
+]
 for i in range(100):
 
     agent = {
@@ -74,7 +88,23 @@ for i in range(100):
 agents_df = pd.DataFrame(agents)
 tickets=[]
 for i in range(1000):
-
+ 
+    created_date = fake.date_between(
+     start_date="-180d",
+     end_date="today"
+)
+    resolution_hours = round(
+      random.uniform(1,48),
+      2
+)
+    closed_date = (
+     datetime.combine(
+        created_date,
+        datetime.min.time()
+     )
+     +
+     timedelta(hours=resolution_hours)
+)
     ticket = {
 
         "TicketID": f"TKT{str(i+1).zfill(5)}",
@@ -88,28 +118,45 @@ for i in range(1000):
                 agents_df["AgentID"].tolist()
             ),
 
-        "ResolutionHours":
-            round(
-                random.uniform(1,48),
-                2
-            ),
+        "ResolutionHours":resolution_hours,
 
         "SLAHours":
             random.choice(
                 [4,8,12,24]
             ),
+            
 
         "Status":
-            random.choice(statuses)
+            random.choice(statuses),
+    
+     "CreatedDate":created_date,
+
+
+    "ClosedDate": 
+    closed_date.date(),
+
+     "Queue":
+    random.choice(queues),
+
+    "Tier":
+    random.choice(tiers),
+
+"EscalatedFlag":
+    random.choice(
+        [True, False]
+    ),   
 
     }
-
+    ticket["BacklogAge"] = (
+    datetime.today().date()
+    -
+    ticket["CreatedDate"]
+).days    
     ticket["SLAMet"] = (
-        ticket["ResolutionHours"]
+    ticket["ResolutionHours"]
         <=
-        ticket["SLAHours"]
+    ticket["SLAHours"]
     )
-
     tickets.append(ticket)
 
 tickets_df = pd.DataFrame(tickets)
